7bbd1c44df62eb1ce1aacf3fc37726294cc9c492
Steven <spartida0002@gmail.com>
2025-03-08 19:20:19 -0800

    Started on navigation bar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,8 +1,31 @@
 import React from 'react'
+import Link from 'next/link'
+import Image from 'next/image'
+
 
 const Navbar = () => {
   return (
-    <div className='text-roboto text-black text-base'>This ia my nav bar Navbar</div>
+    <header className='absolute top-0 z-50 font-roboto w-full'>
+      <nav className='bg-black text-white flex w-auto justify-between items-center px-14 py-4'>
+        <Link href='/'>
+          <Image src="/logoWhite.png" alt='logo' width={64} height={64}></Image>
+        </Link>
+        <div className='flex space-x-4 font-roboto font-base font-normal'>
+          <Link href="/">
+            <span>Services</span>
+          </Link>
+          <Link href="/">
+            <span>Projects</span>
+          </Link>
+          <Link href="/">
+            <span>About</span>
+          </Link>
+          <Link href="/">
+            <span>Contact</span>
+          </Link>
+        </div>
+      </nav>
+    </header>
   )
 }
 
