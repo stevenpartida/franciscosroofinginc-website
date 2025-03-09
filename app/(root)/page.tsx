8b0d241408478bb9b23c05e168844e76b644bf43
Steven <spartida0002@gmail.com>
2025-03-08 22:19:33 -0800

    Build Hero section for desktop

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,46 @@
 import Image from "next/image";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-      <h1 className="text-manrope text-3xl font-bold">HOME</h1>
+    <main className="relative w-full min-h-screen overflow-hidden">
+      <video
+        autoPlay
+        muted
+        loop
+        playsInline
+        className="absolute top-0 left-0 w-full h-full object-cover"
+      >
+        <source src="/HeroVideo.mp4" />
+      </video>
+      <div className="absolute top-0 left-0 w-full h-full bg-gradient-to-b from-[#12121200] to-[#121212] to-98%"></div>
+
+      <section className="absolute sm:bottom-0 sm:left-0 w-full px-14 py-8">
+        <div className="flex justify-center sm:justify-between items-center ">
+          <h1 className="font-manrope text-white uppercase font-extrabold tracking-tighter leading-13 sm:text-6xl">
+            TRUSTED ROOFING <br />
+            SOLUTIONS FOR <br />
+            HOMES & <br />
+            BUSINESSES
+          </h1>
+          <div className="font-roboto text-white flex flex-col space-y-8">
+            <p className="text-sm sm:text-base font-bold leading-5">
+              We use top-quality materials and expert <br />
+              techniques to deliver long-lasting <br />
+              roofing solutions.
+            </p>
+            <div className="flex space-x-4 flex-row">
+              <Link href="/">
+                <Button variant="gradient">Get Free Estimate</Button>
+              </Link>
+              <Link href="/">
+                <Button variant="outline">View Projects</Button>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </section>
+    </main>
   );
 }
---
+++ components/Navbar.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import { Button } from "./ui/button";
+import { ChevronRight } from "lucide-react";
+
+const Navbar = () => {
+  return (
+    <header className="absolute top-0 z-50 font-roboto w-full ">
+      <nav className=" text-white flex w-auto justify-between items-center text-center px-14 py-4">
+        <Link href="/">
+          <Image src="/logoWhite.png" alt="logo" width={64} height={64}></Image>
+        </Link>
+        <div className="flex space-x-8 font-roboto font-base font-normal">
+          <Link href="/">
+            <span>Services</span>
+          </Link>
+          <Link href="/">
+            <span>Projects</span>
+          </Link>
+          <Link href="/">
+            <span>About</span>
+          </Link>
+          <Link href="/">
+            <span>Contact</span>
+          </Link>
+        </div>
+        <Link href="/">
+          <Button variant="gradient" className="font-semibold font-roboto">
+            Free Estimate <ChevronRight />
+          </Button>
+        </Link>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
