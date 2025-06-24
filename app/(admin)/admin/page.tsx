1e8141ab8a5d1a77658a473756d4958f96e4c18b
Steven <106054873+stevenpartida@users.noreply.github.com>
2025-06-24 02:32:11 -0700

    Started working on the admin page

--- app/(admin)/admin/page.tsx
+++ app/(admin)/admin/page.tsx
@@ -1,6 +1,8 @@
 // app/(admin)/admin/page.tsx
 import { redirect } from "next/navigation";
 import { getAuthSession } from "@/lib/auth";
+import { main } from "framer-motion/client";
+import { Button } from "@/components/ui/button";
 
 export default async function AdminPage() {
   const session = await getAuthSession();
@@ -10,8 +12,13 @@ export default async function AdminPage() {
   }
 
   return (
-    <div className="min-h-screen flex items-center justify-center">
-      <h1 className="text-2xl font-bold">This is the admin landing</h1>
-    </div>
+    <main className="flex flex-row min-h-screen w-full">
+      <section className="basis-2/5 flex items-center justify-center bg-green-200">
+        <div>Form</div>
+      </section>
+      <section className="basis-3/5 flex items-center justify-center bg-blue-300">
+        <div>Grid</div>
+      </section>
+    </main>
   );
 }
